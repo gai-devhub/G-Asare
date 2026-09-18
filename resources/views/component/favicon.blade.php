02db688b070bca4dc47f5b0aef57e43843fd8084
gai-devhub <gai.dev.official@gmail.com>
2026-09-18 11:55:47 +0000

    Enhance favicon with both png and ico fallbacks and cache-busting

--- resources/views/component/favicon.blade.php
+++ resources/views/component/favicon.blade.php
@@ -1,3 +1,4 @@
 <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}?v={{ filemtime(public_path('images/logo.png')) }}">
-<link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.png') }}?v={{ filemtime(public_path('images/logo.png')) }}">
+<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ filemtime(public_path('favicon.ico')) }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ filemtime(public_path('favicon.ico')) }}">
 <link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}?v={{ filemtime(public_path('images/logo.png')) }}">
