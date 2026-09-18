942d773b4af4b93f08c05783518bba0cf7c6d93b
gai-devhub <gai.dev.official@gmail.com>
2026-09-18 11:53:45 +0000

    Add global favicon component across all pages including auth and admin

---
+++ resources/views/component/favicon.blade.php
@@ -0,0 +1,3 @@
+<link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}?v={{ filemtime(public_path('images/logo.png')) }}">
+<link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.png') }}?v={{ filemtime(public_path('images/logo.png')) }}">
+<link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}?v={{ filemtime(public_path('images/logo.png')) }}">
